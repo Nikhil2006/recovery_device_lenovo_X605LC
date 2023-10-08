c71862db98db1014fa370d259eb9f87bf71d76ec
Nikhil2006 <nikhilradder9@gmail.com>
2023-10-08 06:30:39 +0530

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,125 @@
+#
+# Copyright 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8953
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyHSL0 
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom 
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 
+BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci 
+BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78af000 
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image 
+BOARD_KERNEL_CMDLINE += loop.max_part=7 
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=false 
+BOARD_KERNEL_CMDLINE += buildvariant=user
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_TAGS_OFFSET := 0x0000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_KERNEL_SEPARATED_DTBO :=
+endif
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8953
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0X04000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0X04000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0XC0000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0X05E2379E00
+BOARD_CACHEIMAGE_PARTITION_SIZE := 0X10000000
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 0X02000000
+BOARD_VENDORIMAGE_PARTITION_SIZE := 0X40000000
+# fs types
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_ROOT_EXTRA_SYMLINKS := /vendor/dsp:/dsp /vendor/firmware_mnt:/firmware
+
+# decryption
+TW_INCLUDE_CRYPTO := true
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+PLATFORM_SECURITY_PATCH := 2099-12-31
+
+# TWRP specific build flags
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_TWRPAPP := true
+TW_EXCLUDE_SUPERSU := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TW_HAS_EDL_MODE := true
+TW_INCLUDE_NTFS_3G := true
+
+# disable mouse cursor
+TW_INPUT_BLACKLIST := "hbtp_vm"
+
+# landscape
+TW_THEME := landscape_hdpi
+TW_ROTATION := 90
+RECOVERY_TOUCHSCREEN_SWAP_XY := true
+RECOVERY_TOUCHSCREEN_FLIP_Y := true
+
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 200
+# TARGET_USE_CUSTOM_LUN_FILE_PATH causes compile-time error in TWRP for 9.0
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/gadget/lun0/file"
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# TWRP device tree for Lenovo Tab M10 FHD REL TB-X605LC (hq_sdm450)
+
+## Device specifications
+
+| Feature                 | Specification                                                                  |
+| :---------------------- | :------------------------------------------------------------------------------|
+| Chipset                 | Qualcomm SDM450 Snapdragon 450 (14 nm)                                         |
+| CPU                     | Octa-core 1.8 GHz Cortex-A53                                                   |
+| GPU                     | Adreno 506                                                                     |
+| Memory                  | 2 GB                                                                           |
+| Shipped Android Version | 9 (Pie)                                                                        |
+| Storage                 | 32 GB eMMC 5.1                                                                 |
+| SIM                     | Single SIM (Nano-SIM)                                                          |
+| Battery                 | Li-Po 7000 mAh, non-removable                                                  |
+| Display                 | Resolution: 1200 x 1920 pixels, 16:10 ratio (~224 ppi density)                 |
+| Rear Camera             | 8 MP, AF                                                                       |
+| Front Camera            | 5 MP                                                                           |
+| Sensors                 | Accelerometer                                                                  |
+
+## Device picture
+
+![Lenovo Tab M10 FHD REL](https://fdn2.gsmarena.com/vv/pics/lenovo/lenovo-m10-fhd-rel-1.jpg)
